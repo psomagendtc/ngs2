a58e923b7cfe43ee2caffc43db84b32160d49983
Changuk Kim <changuk.kim@psomagen.com>
2022-11-15 15:49:18 +0000

    change the daemon job command path

--- srv/account/pw.php
+++ srv/account/pw.php
@@ -8,7 +8,7 @@ if(file_exists($password_filename)){
     $passwords=explode("\n", file_get_contents($password_filename));
     if($token==md5($passwords[0]).md5($passwords[1])){
         $command="python ".dirroot."/__u__/make_password.py '".str_replace("'", "'\\''", $pw)."' > '"._CONFIGS('dataroot')."/".$id."/password'";
-        $command_filename=tempnam(_CONFIGS('dataroot'), 'command_');
+        $command_filename=tempnam(_CONFIGS('dataroot').'/ngs2_password_reset_daemon_job', 'command_');
         unlink($command_filename);
         $command_filename.='.sh';
         file_put_contents($command_filename, $command);
