146266ff744ccbcfd939a2117c0233186ad9f438
Changuk Kim <changuk.kim@psomagen.com>
2023-01-27 20:35:18 +0000

    change filelist to exclude the dir

--- srv/data/filelist.php
+++ srv/data/filelist.php
@@ -3,6 +3,8 @@ $project=$input['project'];
 $sample=$input['sample'];
 $linkroot=_CONFIGS('linkroot');
 foreach(glob(userdir()."/{$project}/{$sample}/*") as $filename){
-    array_push($output, ['name'=>basename($filename), 'size'=>filesize($filename)]);
+    if(is_file($filename)){
+        array_push($output, ['name'=>basename($filename), 'size'=>filesize($filename)]);
+    }
 }
 ?>
